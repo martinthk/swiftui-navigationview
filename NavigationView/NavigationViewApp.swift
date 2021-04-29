20cdc907cdb5e5e5b15aee76b240ed33e81980d5
Martin T <martintom.work@gmail.com>
2021-04-29 09:15:42 +0800

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Martin Tom on 29/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
